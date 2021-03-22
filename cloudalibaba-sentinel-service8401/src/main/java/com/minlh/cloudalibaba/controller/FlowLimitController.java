57dcfe1fbcf7dd38b902791231f21f92d8fcc01c
minlinhai <minlinhai@outlook.com>
2021-03-22 18:41:11 +0800

    20210322提交开发代码V01

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/minlh/cloudalibaba/controller/FlowLimitController.java
@@ -0,0 +1,17 @@
+package com.minlh.cloudalibaba.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FlowLimitController {
+    @GetMapping(value = "/testA")
+    public String testA() {
+        return "------------testA";
+    }
+
+    @GetMapping(value = "/testB")
+    public String testB() {
+        return "------------testB";
+    }
+}
