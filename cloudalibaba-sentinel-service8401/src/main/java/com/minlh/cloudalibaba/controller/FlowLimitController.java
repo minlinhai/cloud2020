db06c077f0668e970cde6bf7011011d350575ae7
minlinhai <minlinhai@outlook.com>
2021-03-25 16:55:04 +0800

    20210325提交开发代码V01

--- cloudalibaba-sentinel-service8401/src/main/java/com/minlh/cloudalibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/minlh/cloudalibaba/controller/FlowLimitController.java
@@ -1,5 +1,6 @@
 package com.minlh.cloudalibaba.controller;
 
+import ch.qos.logback.core.util.TimeUtil;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +14,16 @@ public class FlowLimitController {
     @GetMapping(value = "/testB")
     public String testB() {
         return "------------testB";
+    }
+
+    @GetMapping(value = "/testD")
+    public String testD() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("test 测试RT");
+        return "------------testD";
     }
 }
